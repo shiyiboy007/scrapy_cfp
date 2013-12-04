914c04951fc4431bdbc5c859f97f4b8254e8114c
shiyiboy007 <yshi.telecom@gmail.com>
2013-12-04 16:59:17 +0000

    update readme and add Config module

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This project is a test project aiming at learning to develope useful datamining 
 The purpose is collecting "Call for papers" information of upcoming security conferences.
 
 The developement environement is python2.7 along with some useful tools such as virtualenv, pip, ipython and vim.
+
+#P.S
+Activate syntax coloring in vim:
+==> echo "syntax on" >> $HOME/.vimrc
+Install Ipython in virtualenv used so that scrapy shell will use it instead:
+==> pip install ipython
---
+++ lib/Config.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""Manage configuration ini files and cmd line args"""
+
+# Import from the standard library
+import os
+import sys
+from ConfigParser import RawConfigParser
+
+CONF_FILES = ["query.ini"]
+
+
+def check():
+    "Check that we are in a project"
+    # check that that we are in a crawling project
+    for filename in CONF_FILES:
+        msg = ("\n  You're not in a crawling project '{}' file is missing\n"
+               "  Please initialize a project first\n")
+        if not os.path.isfile(filename):
+            print msg.format(filename)
+            sys.exit()
+
+
+class Config(object):
+    """ Load ini files and store arguments info"""
+
+    def __init__(self):
+        self.query = None
+        self.argline = None
+        check()
+        self.load()
+
+    def load(self):
+        "Load ini files"
+        self.query = RawConfigParser()
+        self.argline = RawConfigParser()
+
+        self.argline.add_section('argline')
+
+        queryfilename = CONF_FILES
+        self.query.read(cybername)
