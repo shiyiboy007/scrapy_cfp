8f3ed4e73b15aeb68b8094efb4f0c7d494f9f410
shiyiboy007 <yshi.telecom@gmail.com>
2013-12-04 12:06:56 +0000

    adding files cfp

---
+++ cfp/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for cfp project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cfp'
+
+SPIDER_MODULES = ['cfp.spiders']
+NEWSPIDER_MODULE = 'cfp.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cfp (+http://www.yourdomain.com)'
